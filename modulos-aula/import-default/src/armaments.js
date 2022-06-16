fef3021bfc1ba6cf4c20b9515a56af242880a4c2
Emilio Butzlaff <emiliobutz@gmail.com>
2022-06-16 11:06:06 -0300

    New files

---
+++ modulos-aula/import-default/src/armaments.js
@@ -0,0 +1,4 @@
+export default {
+    laser: ["Phasers", "Tórpedos de Fóton"],
+    explosion: ["Canhão", "Torpedos de TNT"]
+}
---
+++ modulos-aula/import-default/src/index.js
@@ -0,0 +1,7 @@
+import Spaceship from "./spaceship";
+import armamentKind from "./armaments";
+
+const spaceship = new Spaceship('USS Enterprise', 'James Tiberius Kirk',
+armamentKind.laser, 'Shields')
+
+console.log(spaceship)
