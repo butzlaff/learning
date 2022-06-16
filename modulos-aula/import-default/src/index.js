81d8df673faab68d2dbbac4118eedf023a8e8efb
Emilio Butzlaff <emiliobutz@gmail.com>
2022-06-16 11:50:13 -0300

    New files++

--- modulos-aula/import-default/src/index.js
+++ modulos-aula/import-default/src/index.js
@@ -1,9 +1,13 @@
 import Spaceship from "./spaceship";
 import armamentKind from "./armaments";
-import { laserDefenses, steelDefenses } from "./defenses";
+import { laserDefenses } from "./defenses";
+import basicDefenses from "./defenses"
 
 
 const spaceship = new Spaceship('USS Enterprise', 'James Tiberius Kirk',
 armamentKind.laser, laserDefenses)
 
+const basicSpaceship = new Spaceship('Nave Básica', 'Astronauta Zin', [], basicDefenses)
+
 console.log(spaceship)
+console.log(basicSpaceship)
